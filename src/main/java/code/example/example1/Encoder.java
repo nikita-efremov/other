35468bc91c24a14d194c5d0f260b0145a6eee01c
nikitaherr <nikita.herr@gmail.com>
2016-01-03 14:52:36 +0300

    Encoder sample

--- src/main/java/code/example/example1/Encoder.java
+++ src/main/java/code/example/example1/Encoder.java
@@ -1,7 +1,76 @@
 package code.example.example1;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
 /**
  * Created by herr on 02.01.2016.
  */
 public class Encoder {
+
+    public static void main(String[] args) {
+        try {
+
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+            keyGenerator.init(56);
+            Key secretKey = keyGenerator.generateKey();
+
+            byte[] key = secretKey.getEncoded();
+
+            FileInputStream fis = new FileInputStream("original.txt");
+            FileOutputStream fos = new FileOutputStream("encrypted.txt");
+            encrypt(secretKey, fis, fos);
+
+            FileInputStream fis2 = new FileInputStream("encrypted.txt");
+            FileOutputStream fos2 = new FileOutputStream("decrypted.txt");
+            decrypt(secretKey, fis2, fos2);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void encrypt(Key key, InputStream is, OutputStream os)
+            throws Throwable {
+        encryptOrDecrypt(key, Cipher.ENCRYPT_MODE, is, os);
+    }
+
+    public static void decrypt(Key key, InputStream is, OutputStream os)
+            throws Throwable {
+        encryptOrDecrypt(key, Cipher.DECRYPT_MODE, is, os);
+    }
+
+    public static void encryptOrDecrypt(Key key, int mode, InputStream is,
+                                        OutputStream os) throws Throwable {
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+
+        if (mode == Cipher.ENCRYPT_MODE) {
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            CipherInputStream cis = new CipherInputStream(is, cipher);
+            doCopy(cis, os);
+        } else if (mode == Cipher.DECRYPT_MODE) {
+
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            CipherOutputStream cos = new CipherOutputStream(os, cipher);
+            doCopy(is, cos);
+        }
+    }
+
+    public static void doCopy(InputStream is, OutputStream os)
+            throws IOException {
+        byte[] bytes = new byte[64];
+        int numBytes;
+        while ((numBytes = is.read(bytes)) != -1) {
+            os.write(bytes, 0, numBytes);
+        }
+        os.flush();
+        os.close();
+        is.close();
+    }
+
 }
